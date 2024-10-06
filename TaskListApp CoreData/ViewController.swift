a670bb7aeb41b6dc623aa35487603f0b1114935e
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-06 19:00:57 +0200

    Initial Commit

---
+++ TaskListApp CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskListApp CoreData
+//
+//  Created by user246073 on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
